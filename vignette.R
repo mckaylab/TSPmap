10a0d0d4c2d26c46b16abf4f871eee1e59637b76
Grey Monroe <greymonroe@gmail.com>
2016-05-11 09:33:58 -0600

    removed , PACKAGE = "TSPmap" from finddups working on vignetter

---
+++ vignette.R
@@ -0,0 +1,7 @@
+
+# Install TSPmap ----------------------------------------------------------
+
+#install.packages(devtools) #unhash if you do not have devtools installed
+library(devtools)
+install_bitbucket("greymonroe/TSPmap")
+library(TSPmap)
