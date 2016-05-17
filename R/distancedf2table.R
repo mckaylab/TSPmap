722b1153667102c6c2ecec444c73f42398d89b22
Grey Monroe <greymonroe@gmail.com>
2016-05-17 14:30:22 -0600

    docs

--- R/distancedf2table.R
+++ R/distancedf2table.R
@@ -1,10 +1,10 @@
 #' create table of marker distances and groups
 #'
-#' converts distancedf into table that can be converted into a map object by qtl::table2map
-#'  @usage distancedf2table(distancedf)
-#'  @param distancedf dataframe containing markernames (markername) distances (`Haldane (cM)`) and groups (groupname) of markers
-#'  @return table with chr=linkage group, pos=distance, and row.names = marker names
-#'  @export
+#' @description converts distancedf into table that can be converted into a map object by qtl::table2map
+#' @usage distancedf2table(distancedf)
+#' @param distancedf dataframe containing markernames (markername) distances (`Haldane (cM)`) and groups (groupname) of markers
+#' @return table with chr=linkage group, pos=distance, and row.names = marker names
+#' @export
 distancedf2table<-function(distancedf){
 markertable<-data.frame(chr=distancedf$groupname, pos=as.character(distancedf$`Haldane (cM)`))
 rownames(markertable)<-distancedf$markername
--- R/reorderMarkers.R
+++ R/reorderMarkers.R
@@ -1,14 +1,16 @@
 #' Reorder raw marker data
 #'
 #' Reorders raw markerdata and adds linkage group and marker position based on TSPmap results, if written as .csv can be imported to R/qtl by qtl:read.cross()
-#'  @usage reorderMarkers(rawdata, distancedf)
-#'  @param rawdata markerdata used to create map
-#'  @param distancedf dataframe containing markernames (markername) distances (`Haldane (cM)`) and groups (groupname) of markers
-#'  @return data.frame of markers properly ordered with linkage group and position
-#'  @export
+#' adds a numbered index column before genotype matrix
+#' @usage reorderMarkers(rawdata, distancedf)
+#' @param rawdata markerdata used to create map
+#' @param distancedf dataframe containing markernames (markername) distances (`Haldane (cM)`) and groups (groupname) of markers
+#' @return data.frame of markers properly ordered with linkage group and position
+#' @export
 reorderMarkers<-function(rawdata, distancedf){
 orderedmarkers<-as.matrix(rawdata[,as.character(distancedf$markername)])
 orderedmarkers<-data.frame(orderedmarkers, stringsAsFactors=FALSE)
 orderedmarkers<-rbind(as.character(distancedf$groupname), as.character(distancedf$`Haldane (cM)`), orderedmarkers)
+orderedmarkers<-data.frame(index=c("","",1:(nrow(rawdata))), orderedmarkers)
 return(orderedmarkers)
 }
